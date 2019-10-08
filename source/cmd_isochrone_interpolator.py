39446d64e1357e26a4526b81dbf639454ffd3d40
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2019-10-08 15:59:23 -0500

    Implemented interpolation among CMD downloade isochrone grids.

---
+++ source/cmd_isochrone_interpolator.py
@@ -0,0 +1,132 @@
+"""Define a class that interpolates within a CMD isochrone grid."""
+
+import scipy
+import scipy.interpolate
+
+class IsochroneFileIterator:
+    """
+    Iterate over the sections of the isochrone grid file with line generators.
+
+    Attributes:
+        _isochrone:    The opened isochrone file to iterate over.
+
+        _line:    The last line read from the file.
+    """
+
+    def __init__(self, isochrone_fname):
+        """Create the iterator."""
+
+        self._isochrone = open(isochrone_fname, 'r')
+        self._line = ''
+        while not self._line.startswith('# Zini'):
+            self._line = self._isochrone.readline()
+            assert self._line[0] == '#'
+
+
+    def __enter__(self):
+        """Just return self."""
+
+        return self
+
+    def __iter__(self):
+        """Just return self."""
+
+        return self
+
+    def __next__(self):
+        """Return a generator to the next section in the isochrone grid."""
+
+        def section_line_generator():
+            """Generator over lines of a section of the isochrone grid file."""
+
+            assert self._line[0] == '#'
+            yield self._line
+            self._line = self._isochrone.readline()
+            while self._line[0] != '#':
+                self._line = self._isochrone.readline()
+                if self._line == '':
+                    return
+                yield self._line
+
+        while self._line:
+            return section_line_generator()
+        raise StopIteration()
+
+    def __exit__(self, *_):
+        """Close the underlying isochrone file."""
+
+        self._isochrone.close()
+
+class CMDInterpolator:
+    """
+    Use interpolation to estimate quantities at any mass/[Fe/H] from a CMD grid.
+
+    Attributes:
+        data([scipy field array]):    A the data contained in the isochrone
+            file downloaded from the CMD interface, organized as a list of
+            scipy field arrays, one for each section (corresponding to a
+            single [Fe/H] value)
+    """
+
+    def __init__(self, isochrone_fname):
+        """Interpolate within the given isochrone grid."""
+
+        with IsochroneFileIterator(isochrone_fname) as isochrone:
+            self.data = [scipy.genfromtxt(section, names=True)
+                         for section in isochrone]
+
+        for section_data in self.data:
+            assert scipy.unique(section_data['MH']).size == 1
+
+    def get_interpolated(self, quantity, initial_mass, feh):
+        """
+        Estimate the given quantity for a star of given initial mass & [Fe/H].
+
+        Args:
+            quantity(str):    The quantity to estimate. Should be one of the
+                columns in the CMD isochrone file.
+
+            initial_mass(float):    The initial mass of the star for which to
+                estimate the given quantity in solar masses.
+
+            feh(float):    The metallicity ([Fe/H]) of the star for which to
+                estimate the given quantity
+
+        Return:
+            float:
+                The value of the quantity estimated using linear interpolation
+                against mass for each [Fe/H] value and then against [Fe/H].
+        """
+
+        interp_x = [section_data['MH'][0] for section_data in self.data]
+        interp_y = [
+            scipy.interpolate.interp1d(
+                section_data['Mini'],
+                section_data[quantity],
+                bounds_error=True,
+                assume_sorted=True
+            )(
+                initial_mass
+            )
+            for section_data in self.data
+        ]
+        return scipy.interpolate.interp1d(interp_x,
+                                          interp_y,
+                                          bounds_error=True,
+                                          assume_sorted=True,
+                                          axis=0)(feh)
+
+    def interpolate(self, quantity):
+        """Return an interplolator over the given quantity."""
+
+        return lambda initial_mass, feh: self.get_interpolated(quantity,
+                                                               initial_mass,
+                                                               feh)
+
+if __name__ == '__main__':
+    interpolator = CMDInterpolator('../data/CMD_2.5Gyr_isochrone.dat')
+    print('2.5Gyr Sun Teff = '
+          +
+          repr(10.0**interpolator.get_interpolated('logTe',
+                                                   scipy.linspace(0.5, 1.0, 10),
+                                                   [-0.1, 0.0, 0.1])))
