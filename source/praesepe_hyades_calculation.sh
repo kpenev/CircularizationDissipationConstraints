e454c5fbd5cf1cccf0382073e0a6e49c7edfcbb7
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2020-05-04 22:06:37 -0500

    Save command for processing Praesepe/Hyades for easy reuse
    QPR-96
    QPR-97

---
+++ source/praesepe_hyades_calculation.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+ulimit -S -m $((2 * 1024 * 1024))
+
+nohup ./calculate_e_Q_grid.py \
+    --num-parallel-processes 20 \
+    --initial-eccentricity 0.5 \
+    --use-binary-stars 'praesepe/hyades' \
+    --progress-pickle progress_praesepe_hyades_einit0.5.pickle \
+    --fallback-initial-eccentricity 0.49 \
+    --fallback-initial-eccentricity 0.51 \
+    > \
+    praesepe_hyades_einit0.5_with_fallback.outerr \
+    2>&1
