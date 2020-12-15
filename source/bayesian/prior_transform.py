17788851a8436dcfc676ec1ea11c8c8332de5859
Kaloyan Penev <kpenev@gmail.com>
2020-12-15 10:20:47 -0600

    Finish defining the prior transform interface along with some defaults

--- source/bayesian/prior_transform.py
+++ source/bayesian/prior_transform.py
@@ -3,100 +3,138 @@
 import scipy
 from scipy.stats import norm
 
+from binary_utils import calculate_secondary_mass
+from evolution_parameters import EvolutionParameters
+
 #The foal is to define simple callable.
 #pylint: disable=too-few-public-methods
-class PriorTransformBase:
+class PriorTransformBase(EvolutionParameters):
     """
     Base class for transforming the unit cube to evolution parameters.
 
     Attrs:
-        prior_info:    dictionary containing:
-
-            * system: the value of the `system_parameters` argument to
-              :meth:`__init__`.
-
-            * dissipation: the value of the `dissipation_parameters` argument to
-              :meth:`__init__`.
-
-            * evolution: the value of the `evolution_parameters` argument to
-              :meth:`__init__`.
-
-        parameter_names_units:    See same name argument to :meth:`__init__`.
+        direct_observables:    See same name argument to :meth:`__init__`.
     """
 
-    _parameter_class_order = ['dissipation', 'evolution', 'system']
+    _priors_order = ['dissipation', 'evolution', 'system']
+
+    def _get_direct_observables(self, unit_cube_values):
+        """Turn the unit-cube values to values for the direct observables."""
+
+        result = dict()
+        cube_index = 0
+        for name, measurement in self.direct_observable_distributions:
+            if measurement is None:
+                result[name] = unit_cube_values[cube_index]
+                cube_index += 1
+            elif getattr(measurement, 'plus_error', 0) == 0:
+                assert getattr(measurement, 'minus_error', 0) == 0
+                result[name] = measurement
+            else:
+                cube_value = unit_cube_values[cube_index]
+                cube_index += 1
+
+                result[name] = (
+                    measurement
+                    +
+                    norm.ppf(cube_value) * (
+                        measurement.minus_error if cube_value < 0.5
+                        else measurement.plus_error
+                    ) * measurement.unit
+                )
+
+        return result
+
+    #Define the intefrace that can be overwritten
+    #pylint: disable=no-self-use
+    #pylint: disable=unused-argument
+    def _get_coupled_parameters(self, direct_observables):
+        """Return evolution params which cannot be calculated independantly."""
+
+        return dict()
+    #pylint: enable=no-self-use
+    #pylint: enable=unused-argument
+
+    @staticmethod
+    def get_secondary_mass(direct_observables):
+        """
+        Return the secondary mass per given direct observables.
+
+        Args:
+            direct_observables(dict):     Specific values (with units) to assume
+                for the directly observable system parameters. It must define at
+                least: `primary_mass`, `orbital_period`, `rv_semi_amplitude`,
+                `eccentricity`, and `inclination`.
+        """
+
+        return calculate_secondary_mass(
+            primary_mass=direct_observables['primary_mass'],
+            orbital_period=direct_observables['orbital_period'],
+            rv_semi_amplitude=direct_observables['rv_semi_amplitude'],
+            eccentricity=direct_observables['eccentricity'],
+            inclination=direct_observables['inclination']
+        )
 
     def __init__(self,
-                 *,
-                 parameter_names_units,
-                 **prior_info):
+                 direct_observable_distributions,
+                 secondary_is_star):
         """
         Set-up the prior transform per the given system and evolution params.
 
         Args:
-            system_parameters:    Object with attributes giving the directly
-                observed properties of the system (e.g. RV semi-amplitude
-                instead of secondary mass), complete with units and error bars.
-
-            evolution_parameters:     All non-system specific parameters
-                required to fully specify the evolution (see
-                :attr:`LogLikelihoodBase.parameter_names_units`), complete with
-                units and error bars.
-
-            dissipation_parameters:    All parameters specifying the dissipation
-                (see :attr:`LogLikelihoodBase.parameter_names_units`), complete
-                with units and error bars.
-
-            parameter_names_units:    See
-                :attr:`LogLikelihoodBase.parameter_names_units`
+            direct_observable_distributions(2-tuple):     The names and
+                distributions of the directly observable parameters we will
+                sample directly from. The distributions are assumed i.i.d.
+                following a normal distribution with possibly different positive
+                and negative standard deviations given by the `plus_error` and
+                `minus_error` attributes to each value respectively. Values must
+                have units if not unitless. If errors are zero or unspecified,
+                the corresponding parameter is assumed to have a fixed values
+                and does not consume a unit cube entry. If an observable has a
+                value of None, it is set directly equal to a unique unit cube
+                variable.
 
         Returns:
             None
         """
 
-        self.prior_info = prior_info
-        self.parameter_names_units = parameter_names_units
+        super().__init__(secondary_is_star)
+        self.direct_observable_distributions = direct_observable_distributions
 
     def __call__(self, unit_cube_values):
-        """
-        Return an array of the parameter values for evolving the system.
+        """Return an array of the parameter values for evolving the system."""
 
-        The order and units of the parameters should be exactly as specified by
-        :attr:`parameter_names_units`.
-        """
+        transformed_values = scipy.empty(shape=(len(self.parameter_order),),
+                                         fill_value=scipy.nan,
+                                         dtype=float)
 
-        parameter_values = scipy.empty(shape=(len(self.parameter_names_units),),
-                                       dtype=float)
+        direct_observables = self._get_direct_observables(unit_cube_values)
 
-        start_param_index = 0
-        cube_index = 0
-        for parameter_class in self._parameter_class_order:
+        for param_index, (param_name, param_units) in enumerate(
+                self.parameter_order
+        ):
+            if param_name in direct_observables:
+                transformed_values[param_index] = direct_observables[param_name]
+            elif hasattr(self, 'get_' + param_name):
+                transformed_values[param_index] = getattr(
+                    self,
+                    'get_' + param_name
+                )(
+                    direct_observables
+                ).to_value(
+                    param_units
+                )
 
-            for param_index, (param_name, units) in enumerate(
-                    self.parameter_names_units[parameter_class],
-                    start_param_index
-            ):
-                measurement = getattr(self.prior_info[parameter_class],
-                                      param_name)
-                if getattr(measurement, 'plus_error', 0) == 0:
-                    assert getattr(measurement, 'minus_error', 0) == 0
-                    parameter_values[param_index] = measurement.to_value(units)
-                else:
-                    cube_value = unit_cube_values[cube_index]
-                    cube_index += 1
-
-                    parameter_values[param_index] = (
-                        measurement
-                        +
-                        norm.ppf(cube_value) * (
-                            measurement.minus_error if cube_value < 0.5
-                            else measurement.plus_error
-                        )
-                    ).to_value(units)
-
-            start_param_index += len(
-                self.parameter_names_units[parameter_class]
+        for param_name, param_value in self._get_coupled_parameters(
+                direct_observables
+        ):
+            (
+                transformed_index,
+                transformed_units
+            ) = self.get_parameter_index_units(param_name)
+            transformed_values[transformed_index] = param_value.to_value(
+                transformed_units
             )
 
-        return parameter_values
+        return transformed_values
 #pylint: enable=too-few-public-methods
