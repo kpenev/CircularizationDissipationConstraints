ec11a1c1708424c8027efacdfc8e43d722d355e7
Kaloyan Penev <kpenev@gmail.com>
2021-03-15 13:44:05 -0500

    Improve handling of multiprocessing output and logging

--- source/bayesian/sampling.py
+++ source/bayesian/sampling.py
@@ -43,7 +43,13 @@ def setup_process(config):
 
     std_out_err_fname = config.std_out_err_fname % fname_substitutions
     ensure_directory(std_out_err_fname)
-    sys.stderr = open(std_out_err_fname, 'w', buffering=1)
+
+    io_destination = os.open(
+        std_out_err_fname,
+        os.O_WRONLY | os.O_TRUNC | os.O_CREAT | os.O_DSYNC
+    )
+    os.dup2(io_destination, 1)
+    os.dup2(io_destination, 2)
 
     logging_fname = config.logging_fname % fname_substitutions
     ensure_directory(logging_fname)
