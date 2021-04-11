0474bc32228f04166dd5f5498d1a94cc9efc670e
Kaloyan Penev <kpenev@gmail.com>
2021-04-11 13:01:06 -0500

    Remove "test_" from auto generated ganymede slurm scripts

--- source/slurm/ganymede/generate_slurm.sh
+++ source/slurm/ganymede/generate_slurm.sh
@@ -4,4 +4,4 @@ sed\
     -e 's%@@SYSTEM@@%'"$SYSTEM"'%g'\
     slurm/ganymede/template.slurm\
     >\
-    ~/projects/git/CircularizationDissipationConstraints/source/slurm/ganymede/test_${SYSTEM}.slurm
+    ~/projects/git/CircularizationDissipationConstraints/source/slurm/ganymede/${SYSTEM}.slurm
