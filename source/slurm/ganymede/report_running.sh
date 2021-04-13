86c5a1480190f7d562f2ff8627bec977879ebd61
Kaloyan Penev <kxp174430@ganymede.utdallas.edu>
2021-04-13 07:56:40 -0500

    Add bash script to report what is running and its progress on ganymede

---
+++ source/slurm/ganymede/report_running.sh
@@ -0,0 +1,28 @@
+SLURM_DIR=~/projects/git/CircularizationDissipationConstraints/source/slurm/ganymede
+for f in ${SLURM_DIR}/NGC*.slurm; do 
+    sys=$(basename ${f%.slurm})
+    if [ "$sys" == "NGC188_4618" ]; then
+        chain='chain00010'
+    else
+        chain='chain00000'
+    fi
+    nsamples=$(\
+        h5dump -a "${chain}/iteration" ${sys}_mcmc_samples.h5\
+        |\
+        grep '(0)'\
+        |\
+        awk '{print $2;}'\
+    )
+    echo -n "$sys ($nsamples): $(squeue -n $sys -o '%.15i %.9P %.15j %.2t %.10M %R'|grep NGC)" 
+    if [ \
+            "$sys" == "NGC188_5601" \
+            -o \
+            "$sys" == "NGC188_4999" \
+            -o \
+            "$sys" == "NGC188_8775"\
+    ]; then
+        echo "-----------------------------------------------------------------"
+    else
+        echo ""
+    fi
+done
