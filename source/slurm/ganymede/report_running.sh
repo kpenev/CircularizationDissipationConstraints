aaa9ad93b42edfc0fd7b0b24f4439cf49474aa82
Kaloyan Penev <kxp174430@ganymede.utdallas.edu>
2021-04-21 16:29:17 -0500

    Mark another system as complete

--- source/slurm/ganymede/report_running.sh
+++ source/slurm/ganymede/report_running.sh
@@ -7,7 +7,7 @@ for f in ${SLURM_DIR}/NGC*.slurm; do
         chain='chain00000'
     fi
     nsamples=$(\
-        h5dump -a "${chain}/iteration" ${sys}_mcmc_samples.h5\
+        h5dump -a "${chain}/iteration" ~/${sys}_mcmc_samples.h5\
         |\
         grep '(0)'\
         |\
@@ -15,21 +15,23 @@ for f in ${SLURM_DIR}/NGC*.slurm; do
     )
     echo -n "$(awk -v sys="$sys" -v nsamples="$nsamples" 'BEGIN{printf("%23s (%3d):", sys, nsamples);}') $(squeue -n $sys -o '%.8i %.9P %.18j %.2t %.10M %R'|grep NGC)" 
     if [ \
+            "$sys" == "NGC188_4289" \
+            -o \
+            "$sys" == "NGC188_4618" \
+            -o \
+            "$sys" == "NGC188_4904"\
+            -o \
+            "$sys" == "NGC188_4999" \
+            -o \
             "$sys" == "NGC188_5463" \
             -o \
             "$sys" == "NGC188_5601" \
             -o \
-            "$sys" == "NGC188_4618" \
-            -o \
-            "$sys" == "NGC188_4999" \
+            "$sys" == "NGC188_5733"\
             -o \
             "$sys" == "NGC188_8775"\
             -o \
             "$sys" == "NGC188_880"\
-            -o \
-            "$sys" == "NGC188_4904"\
-            -o \
-            "$sys" == "NGC188_5733"\
             -o \
             "$sys" == "NGC6819_33002"\
             -o \
