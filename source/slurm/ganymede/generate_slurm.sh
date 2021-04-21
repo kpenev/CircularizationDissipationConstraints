74bb8e25bc244c4099ae5a881457d319563457f8
Kaloyan Penev <kpenev@gmail.com>
2021-04-20 21:03:16 -0500

    Generalize ganymede slurm generator to allow different config flavors

--- source/slurm/ganymede/generate_slurm.sh
+++ source/slurm/ganymede/generate_slurm.sh
@@ -1,9 +1,15 @@
 SYSTEM=$1
+FLAVOR=$2
+
+if [ "$FLAVOR" == "" ]; then
+    FLAVOR="constQ"
+fi
 
 SLURM_DIR=~/projects/git/CircularizationDissipationConstraints/source/slurm
 
 sed\
     -e 's%@@SYSTEM@@%'"$SYSTEM"'%g'\
+    -e 's%@@FLAVOR@@%'"$FLAVOR"'%g'\
     ${SLURM_DIR}/ganymede/template.slurm\
     >\
-    ${SLURM_DIR}/ganymede/${SYSTEM}.slurm
+    ${SLURM_DIR}/ganymede/${SYSTEM}_${FLAVOR}.slurm
