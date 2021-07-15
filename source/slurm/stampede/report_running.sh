7c53d3a4dd205a7ca4ffaa174b2e6ff776c78a42
Kaloyan Penev <kpenev@login2.stampede2.tacc.utexas.edu>
2021-07-15 05:18:59 -0500

    Allow disabling slurm scripts for reporting purposes

--- source/slurm/stampede/report_running.sh
+++ source/slurm/stampede/report_running.sh
@@ -1,17 +1,21 @@
 SLURM_DIR=~/projects/git/CircularizationDissipationConstraints/source/slurm/stampede
 
-DUMMY="........................................................................"
-
-for f in ${SLURM_DIR}/NGC*.slurm ${SLURM_DIR}/M35*.slurm; do
+for f in ${SLURM_DIR}/NGC*.slurm ${SLURM_DIR}/M35*.slurm ${SLURM_DIR}/*.slurm.disabled; do
     CLUSTER=$(echo $(basename $f)|sed -e 's%\(NGC[0-9]*\)_.*%\1%' -e 's%\(M35\)_.*%\1%')
     CHAIN='chain00000'
     JOBNAME=$(grep '^#SBATCH -J' $f|awk '{print $3;}')
 
-    JOB_INFO=$(
-        squeue -n $JOBNAME -o '%.15i %.9P %.15j %.2t %.10M %R'\
-        |\
-        egrep 'NGC|M35'
-    )
+    if [ "${f#*.slurm}" == ".disabled" ] ; then
+        JOB_INFO="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        DUMMY="$JOB_INFO"
+    else
+        JOB_INFO=$(
+            squeue -n $JOBNAME -o '%.15i %.9P %.15j %.2t %.10M %R'\
+            |\
+            egrep 'NGC|M35'
+        )
+        DUMMY="........................................................................"
+    fi
 
     echo $JOBNAME
 
