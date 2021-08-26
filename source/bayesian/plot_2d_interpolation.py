4cc1e6494de220d2c9099abe570f0757c4d8f61d
Kaloyan Penev <kpenev@gmail.com>
2021-08-26 18:06:36 -0500

    Found good structure for class approximating 2D functions

--- source/bayesian/plot_2d_interpolation.py
+++ source/bayesian/plot_2d_interpolation.py
@@ -1,83 +1,242 @@
 """Plotting for `tuned_2d_interpolation` module."""
 
+import inspect
+from abc import ABC, abstractmethod
+
 from matplotlib import pyplot
 import numpy
 
-def plot_interpolation_performance(*,
-                                    calculated_values,
-                                    interpolated_values,
-                                    feh_grid,
-                                    mass_grid,
-                                    interp_data,
-                                    title):
-    """Show plot of how the interpolation performs as grid is refined."""
+#Intended just as base class. Public methods come from children
+#pylint: disable=too-few-public-methods
+class Plot2DInterpolation(ABC):
+    """Implement plotting of the progress of 2D interpolation tuning."""
 
-    def plot_interp_performance(difference, max_discrepancy_ind):
-        """Create multi-panel plot showing the current interpolation."""
-
-        pyplot.subplot(221)
-        plot_feh = get_plot_grid(feh_grid)
-        plot_masses = get_plot_grid(mass_grid)
-        pyplot.pcolormesh(plot_masses,
-                          plot_feh,
-                          calculated_values,
-                          edgecolors='none')
-        pyplot.xlabel(r'$M_\star$ [$M_\odot$]')
-        pyplot.ylabel('[Fe/H]')
-        pyplot.title('Calculated')
-        pyplot.colorbar()
-
-        pyplot.subplot(222)
-        pyplot.pcolormesh(plot_masses,
-                          plot_feh,
-                          interpolated_values,
-                          edgecolors='none')
-        pyplot.xlabel(r'$M_\star$ [$M_\odot$]')
-        pyplot.ylabel('[Fe/H]')
-        pyplot.title('Interpolated')
-        pyplot.colorbar()
-
-        pyplot.subplot(223)
-        pyplot.pcolormesh(plot_masses,
-                          plot_feh,
-                          difference,
-                          edgecolors='none')
-        pyplot.xlabel(r'$M_\star$ [$M_\odot$]')
-        pyplot.ylabel('[Fe/H]')
-        pyplot.title('Calculated - Interpolated')
-        pyplot.colorbar()
-
-        pyplot.subplot(224)
-        pyplot.plot(mass_grid,
-                    difference[max_discrepancy_ind[0], : ],
-                    '.r')
-        pyplot.xlabel(r'$M_\star$ [$M_\odot$]')
-        pyplot.ylabel('calc - interp')
-
-        pyplot.twiny()
-        pyplot.plot(feh_grid,
-                    difference[ :, max_discrepancy_ind[1]],
-                    '.b')
-        pyplot.xlabel('[Fe/H]')
-
-        pyplot.suptitle(title)
-
-        self._handle_debug_plot(title=title)
-
-    difference = calculated_values - interpolated_values
-    max_discrepancy_ind = numpy.unravel_index(
-        numpy.argmax(
-            numpy.absolute(difference),
-            axis=None
-        ),
-        calculated_values.shape
-    )
-    plot_interp_details(difference, max_discrepancy_ind, 'mass')
-    plot_interp_details(difference, max_discrepancy_ind, 'feh')
-
-    plot_interp_performance(difference, max_discrepancy_ind)
-
-#pylint: enable=too-many-statements
+    @property
+    def _x_grid(self):
+        raise NotImplementedError
 
 
+    @property
+    def _y_grid(self):
+        raise NotImplementedError
 
+
+    #Intended to be overwritten by child classes
+    #pylint: disable=missing-function-docstring
+    @property
+    def configuration(self):
+        raise NotImplementedError
+    #pylint: enable=missing-function-docstring
+
+
+    #This is sufficiently simple.
+    #pylint: disable=too-many-arguments
+    @abstractmethod
+    def _interpolate(self, x_grid, y_grid, values, target_x, target_y):
+        raise NotImplementedError
+    #pylint: enable=too-many-arguments
+
+    def _handle_debug_plot(self, **fname_substitutions):
+        """Either save of display the currently set-up plot."""
+
+        for caller in inspect.stack()[1:]:
+            if caller.function.startswith('_plot_'):
+                break
+
+        #This should fail if caller remains undefined
+        #pylint: disable=undefined-loop-variable
+        caller = caller.function
+        assert caller.startswith('_plot_')
+        #pylint: enable=undefined-loop-variable
+
+        filename = (
+            self._debug_plots[caller[len('_plot_'):]]
+            %
+            dict(
+                fname_substitutions,
+                grid_refinement_i=self._grid_refinement_iteration
+            )
+        )
+        if filename:
+            pyplot.savefig(filename)
+            pyplot.clf()
+        else:
+            pyplot.show()
+
+
+    #Sufficient structure provided by sub-functions.
+    #pylint: disable=too-many-statements
+    def _plot_interpolation_performance(self,
+                                        *,
+                                        calculated_values,
+                                        interpolated_values,
+                                        x_grid,
+                                        y_grid,
+                                        interp_data,
+                                        title):
+        """Show plot of how the interpolation performs as grid is refined."""
+
+        if 'interpolation_performance' not in self._debug_plots:
+            return
+
+        def get_plot_grid(grid):
+            """Return a new grid with point 1/2 between input grid."""
+
+            result = numpy.empty(shape=(grid.size + 1), dtype=grid.dtype)
+            result[1:-1] = 0.5 * (grid[1:] + grid[:-1])
+            result[0] = 2.0 * grid[0] - result[1]
+            result[-1] = 2.0 * grid[-1] - result[-2]
+            return result
+
+
+        def plot_interp_details(difference,
+                                max_discrepancy_ind,
+                                direction):
+            """Show the interpolation in mass."""
+
+            assert direction in 'xy'
+
+            interp_grid = getattr(self, '_%s_grid' % direction)
+            interp_x = numpy.linspace(interp_grid[0], interp_grid[-1], 100)
+            interp_y = self._interpolate(
+                self._x_grid[ : : 2],
+                self._y_grid[ : : 2],
+                interp_data,
+                (
+                    x_grid[max_discrepancy_ind[0]: max_discrepancy_ind[0] + 1]
+                    if direction == 'y' else
+                    interp_x
+                ),
+                (
+                    interp_x
+                    if direction == 'y' else
+                    y_grid[
+                        max_discrepancy_ind[1]
+                        :
+                        max_discrepancy_ind[1] + 1
+                    ]
+                )
+            ).flatten()
+            main_plot = pyplot.subplot(211)
+
+            plot_slice = (
+                numpy.s_[max_discrepancy_ind[0], :]
+                if direction == 'y' else
+                numpy.s_[:, max_discrepancy_ind[1]]
+            )
+            pyplot.plot(interp_x, interp_y, '-r', linewidth=0.25)
+
+            pyplot.plot(
+                interp_grid[ : : 2],
+                interp_data[plot_slice],
+                '.r',
+                label='interp input'
+            )
+
+            plot_grid = y_grid if direction == 'y' else x_grid
+            pyplot.plot(
+                plot_grid,
+                calculated_values[plot_slice],
+                '.g',
+                label='calculated'
+            )
+            pyplot.plot(
+                plot_grid,
+                interpolated_values[plot_slice],
+                '.b',
+                label='interpolated'
+            )
+            pyplot.legend()
+
+            pyplot.subplot(212, sharex=main_plot)
+            pyplot.plot(
+                plot_grid,
+                difference[plot_slice],
+                '.k'
+            )
+            self._handle_debug_plot(
+                title=(
+                    title
+                    +
+                    '_interp_vs_calc_'
+                    +
+                    (
+                        'vM_FeH=%g' % x_grid[max_discrepancy_ind[0]]
+                        if direction == 'y' else
+                        'vFeH_M=%g' % y_grid[max_discrepancy_ind[1]]
+                    )
+                )
+            )
+
+        def plot_interp_performance(difference, max_discrepancy_ind):
+            """Create multi-panel plot showing the current interpolation."""
+
+            pyplot.subplot(221)
+            plot_feh = get_plot_grid(x_grid)
+            plot_masses = get_plot_grid(y_grid)
+            pyplot.pcolormesh(plot_masses,
+                              plot_feh,
+                              calculated_values,
+                              edgecolors='none')
+            pyplot.xlabel(r'$M_\star$ [$M_\odot$]')
+            pyplot.ylabel('[Fe/H]')
+            pyplot.title('Calculated')
+            pyplot.colorbar()
+
+            pyplot.subplot(222)
+            pyplot.pcolormesh(plot_masses,
+                              plot_feh,
+                              interpolated_values,
+                              edgecolors='none')
+            pyplot.xlabel(r'$M_\star$ [$M_\odot$]')
+            pyplot.ylabel('[Fe/H]')
+            pyplot.title('Interpolated')
+            pyplot.colorbar()
+
+            pyplot.subplot(223)
+            pyplot.pcolormesh(plot_masses,
+                              plot_feh,
+                              difference,
+                              edgecolors='none')
+            pyplot.xlabel(r'$M_\star$ [$M_\odot$]')
+            pyplot.ylabel('[Fe/H]')
+            pyplot.title('Calculated - Interpolated')
+            pyplot.colorbar()
+
+            pyplot.subplot(224)
+            pyplot.plot(y_grid,
+                        difference[max_discrepancy_ind[0], : ],
+                        '.r')
+            pyplot.xlabel(r'$M_\star$ [$M_\odot$]')
+            pyplot.ylabel('calc - interp')
+
+            pyplot.twiny()
+            pyplot.plot(x_grid,
+                        difference[ :, max_discrepancy_ind[1]],
+                        '.b')
+            pyplot.xlabel('[Fe/H]')
+
+            pyplot.suptitle(title)
+
+            self._handle_debug_plot(title=title)
+
+        difference = calculated_values - interpolated_values
+        max_discrepancy_ind = numpy.unravel_index(
+            numpy.argmax(
+                numpy.absolute(difference),
+                axis=None
+            ),
+            calculated_values.shape
+        )
+        plot_interp_details(difference, max_discrepancy_ind, 'y')
+        plot_interp_details(difference, max_discrepancy_ind, 'x')
+
+        plot_interp_performance(difference, max_discrepancy_ind)
+    #pylint: enable=too-many-statements
+
+    def __init__(self, debug_plots):
+        """Configure the plots to create."""
+
+        self._debug_plots = debug_plots
+        self._grid_refinement_iteration = None
+#pylint: enable=too-few-public-methods
--- source/test.py
+++ source/test.py
@@ -1,17 +1,149 @@
 #!/usr/bin/env python3
 
-import numpy, scipy
+from ctypes import cdll
+from ctypes.util import find_library
+import os
+import sys
+from multiprocessing import Pool, Queue, Process
+from time import sleep
+from configargparse import ArgumentParser, DefaultsFormatter
+
+from matplotlib import pyplot
+from scipy import stats
+import numpy
+
+from dynesty import NestedSampler, plotting as dyplot
+
+cstdlib = cdll.LoadLibrary(find_library('libc'))
+
+#Work around emcee limitation
+#pylint: disable=too-few-public-methods
+class UnchunkedPool:
+    """Disable chunking in Pool.map."""
+
+    def __init__(self, pool):
+        """Wrap around the given pool's map."""
+
+        self._pool = pool
+
+    def map(self, *args, **kwargs):
+        """Delegate everything to parent, but set chunksize to 1."""
+
+        self._pool.map(*args, **kwargs, chunksize=1)
+#pylint: enable=too-few-public-methods
+
+def prior_transform(unit_cube):
+    """Insert fixd value between each input rv."""
+
+    result = numpy.empty(unit_cube.size * 2 - 1)
+    result[::2] = unit_cube
+    result[2] = stats.rice.ppf(unit_cube[1], b = 0.775, scale=2.0)
+    result[1::2] = numpy.arange(unit_cube.size - 1)
+    return result
+
+def log_likelihood(parameters):
+    """For now just gaussian mean=0.5, sigma=0.1 for the first param."""
+
+    return stats.norm.logpdf(parameters[0], loc=0.5, scale=0.1)
+
+def sample(nrandom):
+    """Create and run the sampler return the resluts."""
+
+    sampler = NestedSampler(log_likelihood,
+                            prior_transform,
+                            ndim=(2*nrandom - 1),
+                            npdim=nrandom,
+                            nlive=5000)
+    sampler.run_nested(dlogz=0.001)
+    return sampler.results
+
+def corner_plots(results):
+    """Create and display a corner plot."""
+
+    dyplot.cornerplot(results, show_titles=True, smooth=20)
+    pyplot.show()
+
+def stdout_redirect():
+    """Demonstrate redirecting stdout of a process to file even from library."""
+
+    destination = os.open(
+        'test_redirect_%(pid)s.out' % dict(pid=os.getpid()),
+        os.O_WRONLY | os.O_TRUNC | os.O_CREAT | os.O_DSYNC
+    )
+    os.dup2(destination, 1)
+    os.dup2(destination, 2)
+
+def test_redirect(value):
+    """Check that stdout is properly redirected."""
+
+    cstdlib.printf(
+        (
+            'Printing from C: %d  pid %d\n'
+            %
+            (value, os.getpid())
+        ).encode('ascii')
+    )
+    print('Printing from python: ' + repr(value) + ' pid: ' + repr(os.getpid()))
+    assert 1==0
+
+def test_multiprocessing_io():
+    """Test the scheme for redirecting multiprocessing I/O."""
+
+    stdout_redirect()
+    stdout_redirect()
+
+    cstdlib.printf(
+        (
+            'C printing from global process (pid: %d)\n'
+            %
+            os.getpid()
+        ).encode('ascii')
+    )
+    print('Python printing from global process (pid: %d)'
+          %
+          os.getpid())
+    with Pool(4, initializer=stdout_redirect) as pool:
+        workers = UnchunkedPool(pool)
+        workers.map(test_redirect, range(10))
+
+def f(x):
+    """Function to run multiprocessing on."""
+
+    print('f(%f)' % x)
+    sleep(x)
+    return x
+
+def worker(input_queue, output_queue):
+    """Get a task and run it for multiprocessing."""
+
+    while True:
+        task = input_queue.get()
+        result = f(task)
+        output_queue.put(result)
 
 if __name__ == '__main__':
-    a = scipy.empty((5,3,7), dtype=int)
-    b = scipy.arange(5).reshape((5,1))
-    for i in range(a.shape[0]):
-        for j in range(a.shape[1]):
-            for k in range(a.shape[2]):
-                a[i,j,k] = 1000 * i + 100 * j + 10 * k
+    tasks = Queue()
+    results = Queue()
 
-    print('a: ' + repr(a) + ', shape = ' + repr(a.shape))
-    print('a.T: ' + repr(a.T) + ', shape = ' + repr(a.T.shape))
-    print('b: ' + repr(b))
-    print('(a.T + b.T).T: ' + repr((a.T + b.T).T))
-    print('b + b: ' + repr(b + b))
+    for i in range(3):
+        tasks.put(numpy.random.rand())
+
+    workers = [Process(target=worker, args=(tasks, results))
+               for i in range(4)]
+
+    for process in workers:
+        process.start()
+
+    sevens_found = 0
+    while sevens_found != 3:
+        print('Sevens found: ' + repr(sevens_found))
+        next_result = results.get()
+        if int(next_result * 10) == 7:
+            print('* ', end='')
+            sevens_found += 1
+        else:
+            tasks.put(numpy.random.rand())
+        print(next_result)
+
+    for process in workers:
+        process.terminate()
