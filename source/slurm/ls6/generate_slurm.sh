dff969d4af95d72ac4030d8399b7915aa7e3d285
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2022-09-21 16:04:59 -0500

    Fix bug in LS6 slurm script generation

--- source/slurm/ls6/generate_slurm.sh
+++ source/slurm/ls6/generate_slurm.sh
@@ -13,8 +13,10 @@ echo "Generating $CLUSTER slurm script for LS6"
 
 if [ "$CLUSTER" == "W19" ]; then
     CONFIG_FNAME="W19"
+    SAMPLER="sample_windemuth_et_al.py "
 else
     CONFIG_FNAME="open_cluster"
+    SAMPLER="sample_sb1.py ${CLUSTER}_"
 fi
 
 CONFIG_FNAME="${CONFIG_FNAME}_${SAMPLING_MODE}.cfg"
@@ -34,6 +36,7 @@ sed\
     -e 's%@@SYSTEM_LIST@@%'"$SYSTEMS"'%g'\
     -e 's%@@JOINED_SYSTEMS@@%'"${JOINED_SYSTEMS}"'%g'\
     -e 's%@@CONFIG_FNAME@@%'"${CONFIG_FNAME}"'%g'\
+    -e 's%@@SAMPLER@@%'"${SAMPLER}"'%g'\
     ${SLURM_DIR}/ls6/template_powerlaw.slurm\
     >\
     ${SLURM_DIR}/ls6/${CLUSTER}_${JOINED_SYSTEMS}_powerlaw.slurm
