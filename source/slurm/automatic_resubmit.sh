9896337a1869938e2d2ae7e19eaf8e278d60d523
Kaloyan Penev <kpenev@login1.ls6.tacc.utexas.edu>
2022-09-25 12:25:46 -0500

    Add script to automatically submit any slurm scripts not already running

---
+++ source/slurm/automatic_resubmit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CLUSTER=$1
+
+HOSTNAME=$(hostname)
+
+if [ "$HOSTNAME" == "login1.ls6.tacc.utexas.edu" ]; then
+	HPC='ls6'
+elif [ "$HOSTNAME" == "login1.stampede2.tacc.utexas.edu" ]; then 
+	HPC='stampede'
+elif [ "$HOSTNAME" == "ganymede.utdallas.edu" ]; then 
+	HPC='ganymede'
+else
+	echo "Unrecognized host name: $HOSTNAME"
+	exit 1
+fi
+
+for slurm in $(ls ~/projects/git/CircularizationDissipationConstraints/source/slurm/$HPC/$CLUSTER/*.slurm 2>/dev/null); do
+	JOBNAME=$(grep '#SBATCH  *-J' $slurm|awk '{print $3;}')
+	if [ "$(squeue -n $JOBNAME -h)" == "" ] ; then 
+		sbatch "$slurm"
+	else 
+		echo "$JOBNAME still running"
+	fi
+done
