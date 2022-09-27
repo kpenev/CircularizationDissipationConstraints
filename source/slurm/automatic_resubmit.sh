9791b561ff4b1113c38e48cc1148669b207f3984
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2022-09-27 10:43:36 -0500

    Add moch archiving to automatic job submission

--- source/slurm/automatic_resubmit.sh
+++ source/slurm/automatic_resubmit.sh
@@ -16,9 +16,21 @@ else
 	exit 1
 fi
 
+OUTPUT_ROOT=/work/05392/kpenev/${HPC}/circularization/${CLUSTER}/sampling_output
+
+
 for slurm in $(ls ~/projects/git/CircularizationDissipationConstraints/source/slurm/$HPC/$CLUSTER/*.slurm 2>/dev/null); do
 	JOBNAME=$(grep '#SBATCH  *-J' $slurm|awk '{print $3;}')
 	if [ "$(squeue -n $JOBNAME -h)" == "" ] ; then 
+        for SYSNAME in $(echo $JOBNAME|tr '_' ' '); do 
+            if [ "$SYSNAME" != "$CLUSTER" ] ; then
+                OLD=${OUTPUT_ROOT}/${SYSNAME}
+                NEW=${OLD}_$(date '+%Y%m%d')
+                echo "Archiving  $OLD -> $NEW.tbz"
+                echo mv ${OLD} ${NEW}
+                echo tar -cjf ${NEW}.tbz ${NEW}
+            fi
+        done
 		sbatch "$slurm"
 	else 
 		echo "$JOBNAME still running"
