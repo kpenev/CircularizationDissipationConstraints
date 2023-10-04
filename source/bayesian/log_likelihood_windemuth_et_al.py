72533ec7e6720dcbb3df6cb6fc861dc9807411f7
Joshua Aaron Schussler <vortebo@kartof>
2023-10-04 11:00:51 -0500

    Debug message changes, added ability to communicate system values to reproduce_system.py, fixed upper limit of integration.

--- source/bayesian/log_likelihood_windemuth_et_al.py
+++ source/bayesian/log_likelihood_windemuth_et_al.py
@@ -12,21 +12,21 @@ from stellar_evolution.manager import StellarEvolutionManager
 from orbital_evolution.transformations import phase_lag
 
 def find_evolution(parameters,envelope_eccentricity=None):
-    print('find_evolution()')
+    #print('find_evolution()')
 
     initial_eccentricity = parameters['initial_eccentricity']
     final_period = parameters['system'].orbital_period
     final_eccentricity = parameters['system'].eccentricity
 
-    print('Initial')
-    print('initial_eccentricity = %s' % (repr(initial_eccentricity)))
-    print('final_period = %s' % (repr(final_period)))
-    print('final_eccentricity = %s' % (repr(final_eccentricity)))
+    #print('Initial')
+    #print('initial_eccentricity = %s' % (repr(initial_eccentricity)))
+    #print('final_period = %s' % (repr(final_period)))
+    #print('final_eccentricity = %s' % (repr(final_eccentricity)))
 
     if initial_eccentricity != 'solve':
         #final_eccentricity = initial_eccentricity**2
         change = initial_eccentricity / 2
-        print('change = %s' % (repr(change)))
+        #print('change = %s' % (repr(change)))
         final_eccentricity = initial_eccentricity - change if (initial_eccentricity - change) >= 0.1 else 0.1
         delta_eccentricity = 0.01
         initial_eccentricity_2 = initial_eccentricity + delta_eccentricity if (initial_eccentricity + delta_eccentricity) <= 0.8 else initial_eccentricity - delta_eccentricity
@@ -35,30 +35,30 @@ def find_evolution(parameters,envelope_eccentricity=None):
     else:
         #initial_eccentricity = final_eccentricity**(1/2) if final_eccentricity < .64 else numpy.nan
         change = final_eccentricity / 2
-        print('change = %s' % (repr(change)))
+        #print('change = %s' % (repr(change)))
         initial_eccentricity = final_eccentricity + change if (final_eccentricity + change) <= 0.8 else 0.8
         delta_eccentricity = 0.01
         final_eccentricity_2 = final_eccentricity + delta_eccentricity if (final_eccentricity + delta_eccentricity) <= 0.8 else final_eccentricity - delta_eccentricity
         change = final_eccentricity_2 / 2
         initial_eccentricity_2 = final_eccentricity_2 + change if (final_eccentricity_2 + change) <= 0.8 else 0.8
 
-    print('After')
-    print('initial_eccentricity = %s' % (repr(initial_eccentricity)))
-    print('final_eccentricity = %s' % (repr(final_eccentricity)))
-    print('initial_eccentricity_2 = %s' % (repr(initial_eccentricity_2)))
-    print('final_eccentricity_2 = %s' % (repr(final_eccentricity_2)))
+    #print('After')
+    #print('initial_eccentricity = %s' % (repr(initial_eccentricity)))
+    #print('final_eccentricity = %s' % (repr(final_eccentricity)))
+    #print('initial_eccentricity_2 = %s' % (repr(initial_eccentricity_2)))
+    #print('final_eccentricity_2 = %s' % (repr(final_eccentricity_2)))
 
     ehat_prime = (final_eccentricity - final_eccentricity_2) / (initial_eccentricity - initial_eccentricity_2)
-    print('ehat_prime = %s' % (repr(ehat_prime)))
+    #print('ehat_prime = %s' % (repr(ehat_prime)))
 
     #if envelope_eccentricity is not None:
     #    final_eccentricity = 0.2#envelope_eccentricity * (9/10)
 
-    print((
-            numpy.array([final_period,final_eccentricity]),
-            numpy.array([ehat_prime,initial_eccentricity])
-        )
-    )
+    # print((
+    #         numpy.array([final_period,final_eccentricity]),
+    #         numpy.array([ehat_prime,initial_eccentricity])
+    #     )
+    # )
     return (
         numpy.array([final_period,final_eccentricity]),
         numpy.array([ehat_prime,initial_eccentricity])
@@ -73,6 +73,7 @@ class LogLikelihoodWindemuth(LogLikelihoodBinaryStars):
                  observed_eccentricity_distro,
                  de_distro,
                  pe_distro,
+                 system,
                  **parent_kwargs):
         """
         Prepare the log-likelihood function.
@@ -97,10 +98,11 @@ class LogLikelihoodWindemuth(LogLikelihoodBinaryStars):
 
         self._de = de_distro
         self._pe = pe_distro
+        self.system = system
 
     def _choose_solver(self, parameters, solver = '1d', final_e = 0):
         """Handle the parameters passed to the log-likelihood function."""
-
+        print('parameters = %s' % (repr(parameters)))
         if solver == '1d':
             parameters['solve'] = True
             parameters['initial_eccentricity'] = 0.8
@@ -118,36 +120,38 @@ class LogLikelihoodWindemuth(LogLikelihoodBinaryStars):
     
     def _likelihood_integrand(self,e,ehat_approx, e_to_be_near):
         ehat_prime = ehat_approx.deriv()
-        print('e = %s' % (repr(e)))
+        #print('e = %s' % (repr(e)))
+        logger = logging.getLogger(__name__)
 
         if ehat_approx.degree() == 2:
             ehat_approx_copy = ehat_approx.copy()
             ehat_approx_copy.coef[0] -= e
             inverse_ehat_approx = numpy.polynomial.polynomial.Polynomial((ehat_approx_copy.coef)).roots()
             inverse_e = numpy.real(inverse_ehat_approx[numpy.isreal(inverse_ehat_approx)])
-            print('e = %s' % (repr(e)))
-            print('inverse_e = %s' % (repr(inverse_e)))
+            #print('e = %s' % (repr(e)))
+            #print('inverse_e = %s' % (repr(inverse_e)))
             for option in inverse_e:
                 if option < 0 or option > 0.8:
                     inverse_e = inverse_e[inverse_e != option]
-            print('New inverse_e = %s' % (repr(inverse_e)))
+            #print('New inverse_e = %s' % (repr(inverse_e)))
             if inverse_e.size == 0:
                 #raise ValueError('No real roots found for ehat_approx_copy (inverse_ehat_approx): %s (%s)',repr(ehat_approx_copy),repr(inverse_ehat_approx))
                 # this will be an error when we're not doing debug
                 #TODO
-                print('No in-range real roots found for ehat_approx_copy (inverse_ehat_approx): %s (%s)' % (repr(ehat_approx_copy),repr(inverse_ehat_approx)))
+                #print('No in-range real roots found for ehat_approx_copy (inverse_ehat_approx): %s (%s)' % (repr(ehat_approx_copy),repr(inverse_ehat_approx)))
+                logger.warning('No in-range real roots found for ehat_approx_copy (inverse_ehat_approx): %s (%s)',repr(ehat_approx_copy),repr(inverse_ehat_approx))
                 inverse_e=numpy.nan
             else:
-                print('inverse_e-e_to_be_near = %s' % (repr(inverse_e-e_to_be_near)))
-                print('numpy.abs(inverse_e-e_to_be_near) = %s' % (repr(numpy.abs(inverse_e-e_to_be_near))))
-                print('numpy.argmin(numpy.abs(inverse_e-e_to_be_near)) = %s' % (repr(numpy.argmin(numpy.abs(inverse_e-e_to_be_near)))))
+                #print('inverse_e-e_to_be_near = %s' % (repr(inverse_e-e_to_be_near)))
+                #print('numpy.abs(inverse_e-e_to_be_near) = %s' % (repr(numpy.abs(inverse_e-e_to_be_near))))
+                #print('numpy.argmin(numpy.abs(inverse_e-e_to_be_near)) = %s' % (repr(numpy.argmin(numpy.abs(inverse_e-e_to_be_near)))))
                 inverse_e = inverse_e[numpy.argmin(numpy.abs(inverse_e-e_to_be_near))]
-            print('chosen inverse_e = %s' % (repr(inverse_e)))
+            #print('chosen inverse_e = %s' % (repr(inverse_e)))
         elif ehat_approx.degree() == 1:
             reverse = numpy.polynomial.polynomial.Polynomial((-ehat_approx.coef[0]/ehat_approx.coef[1],1/ehat_approx.coef[1]))
-            print('reverse = %s' % (repr(reverse)))
+            #print('reverse = %s' % (repr(reverse)))
             inverse_e = reverse(e)
-            print('inverse_e = %s' % (repr(inverse_e)))
+            #print('inverse_e = %s' % (repr(inverse_e)))
         else:
             raise ValueError('ehat_approx should be a polynomial of order 1 or 2. Something is wrong with ehat_approx: %s',repr(ehat_approx))
         
@@ -155,10 +159,10 @@ class LogLikelihoodWindemuth(LogLikelihoodBinaryStars):
             raise ValueError('inverse_e should not be outside range: %s',repr(inverse_e))
         
         result = self._de.pdf(e) * self._pe.pdf((inverse_e)) / ehat_prime(inverse_e)
-        print('self._de.pdf(e) = %s' % (repr(self._de.pdf(e))))
-        print('self._pe.pdf((inverse_e)) = %s' % (repr(self._pe.pdf((inverse_e)))))
-        print('ehat_prime(inverse_e) = %s' % (repr(ehat_prime(inverse_e))))
-        print('result = %s' % (repr(result)))
+        #print('self._de.pdf(e) = %s' % (repr(self._de.pdf(e))))
+        #print('self._pe.pdf((inverse_e)) = %s' % (repr(self._pe.pdf((inverse_e)))))
+        #print('ehat_prime(inverse_e) = %s' % (repr(ehat_prime(inverse_e))))
+        #print('result = %s' % (repr(result)))
         return result
 
     def _calculate_likelihood(self, ehat_approx, e_to_be_near, e_min, e_max): #  ef_max,ei_med,dehat_med
@@ -167,11 +171,18 @@ class LogLikelihoodWindemuth(LogLikelihoodBinaryStars):
         return I
 
     def calculate_log_likelihood(self,
-                                 parameters,
+                                 encoded_parameters,
                                  **other_args):
         """Evaluate the log-likelihood at the given model parameters."""
 
         logger = logging.getLogger(__name__)
+
+        parameters = dict()
+        for big_term in self.parameter_names_units:
+            parameters[big_term] = dict()
+            for i in range(len(self.parameter_names_units[big_term])):
+                parameters[big_term][self.parameter_names_units[big_term][i][0]] = self.get_parameter_value(encoded_parameters, self.parameter_names_units[big_term][i][0])
+        parameters['system'] = self.system
 
         assert 'sample_weights_envelope' in other_args #why? TODO
 
@@ -332,7 +343,7 @@ class LogLikelihoodWindemuth(LogLikelihoodBinaryStars):
         #raise ValueError
         
         integration_min = max(0,De_min)
-        integration_max = min(0.8,max_final_eccentricity)
+        integration_max = min(De_max,max_final_eccentricity)
         likelihood = self._calculate_likelihood(ehat_approx,e_to_be_near,integration_min,integration_max)
         #assert likelihood >= 0
         #if likelihood == 0:
@@ -477,7 +488,7 @@ if __name__ == '__main__':
     bob=LogLikelihoodWindemuth(
         envelope_eccentricity=0.7,
         observed_eccentricity_distro=scipy.stats.uniform(loc=0,scale=0.8),
-        de_distro=W19EccentricityDistribution(12356914,pickle_fname='/home/vortebo/ctime/CircularizationDissipationConstraints/windemuth_eccentricity_distros.pkl'),
+        de_distro=W19EccentricityDistribution(3348093,pickle_fname='/home/vortebo/ctime/CircularizationDissipationConstraints/windemuth_eccentricity_distros.pkl'),
         pe_distro=scipy.stats.uniform(loc=0,scale=0.8),
         powerlaw_dissipation = 5,
         interpolator=interpolator,
