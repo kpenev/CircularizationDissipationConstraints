e58021a816b187d7270786f9439977fafb56f5c0
Joshua Aaron Schussler <jas180011@utdallas.edu>
2024-09-17 14:25:22 -0500

    Taking mass and feh into account before attempting to evolve to an age we cannot reach.

--- source/bayesian/log_likelihood_windemuth_et_al.py
+++ source/bayesian/log_likelihood_windemuth_et_al.py
@@ -292,6 +292,15 @@ class LogLikelihoodWindemuth(LogLikelihoodBinaryStars):
                 logger.debug('max_final_eccentricity < De_min: %s',repr(max_final_eccentricity < De_min))
                 return -numpy.inf
         
+        # Don't try to evolve systems past where we have interpolation information
+        max_age_1 = interpolator('radius', parameters['system'].primary_mass.to(units.M_sun).value, parameters['system'].feh).max_age
+        max_age_2 = interpolator('radius', parameters['system'].secondary_mass.to(units.M_sun).value, parameters['system'].feh).max_age
+        max_age = min(max_age_1,max_age_2, 11)
+        if parameters['system'].age.to(units.Gyr).value > max_age:
+            logger.warning('System age %s is greater than the maximum age %s for the given stellar parameters. Adjusting.',
+                           repr(parameters['system'].age.to(units.Gyr).value), repr(max_age))
+            parameters['system'].age = max_age * units.Gyr
+        
         self.median_e = self._de.ppf(0.5)
 
         De_away_from_zero = De_min > 0
